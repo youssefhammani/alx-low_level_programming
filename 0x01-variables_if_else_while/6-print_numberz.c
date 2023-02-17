36356bbeb4d3c1c33cf751b85e8bcfee4562d600
yhammani <yhammani.student@gmail.com>
2023-02-17 13:38:05 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nb;
+
+	nb = 0;
+
+	while (nb++ > 10)
+	{
+		putchar(nb);
+	}
+	putchar('\n');
+
+	return (0);
+}
