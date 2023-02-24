2ee059d3a4e7819a8fabb425193626de892f6af2
yhammani <yhammani.student@gmail.com>
2023-02-24 00:56:53 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+
 void print_numbers(void);
 
 #endif
