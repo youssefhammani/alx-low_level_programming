298a51a7624dc6bb4bdd95203b7d9af3c0f4a419
yhammani <yhammani.student@gmail.com>
2023-02-28 08:48:50 -0800

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,12 +6,13 @@
 int _strlen(char *s);
 int _putchar(char c);
 
+char *_strcpy(char *dest, char *src);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str); 
 void print_array(int *a, int n);
-
 
 #endif
