71f0cd2a2b1eef3d2cd06162d3daae9c4fc22a93
yhammani <yhammani.student@gmail.com>
2023-02-27 16:30:05 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,11 @@ void puts2(char *str)
 
 	i = 0;
 
-	while (str[i])
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
