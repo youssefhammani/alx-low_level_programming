5eedea570e85b8520930a7233bb2642b31123510
yhammani <yhammani.student@gmail.com>
2023-03-01 05:57:07 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,14 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%d ", i);
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
 		{
-			printf("%d ", i);
+			printf("%d, ", i);
 		}
 	}
 	printf("98\n");
