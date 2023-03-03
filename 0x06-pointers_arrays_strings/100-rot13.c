4744ddde6ef7c8a6b5e56b861af35cdde29c56bf
yhammani <yhammani.student@gmail.com>
2023-03-03 14:29:13 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,19 +7,19 @@
  *Return: pointer to encoded string.
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i, j;
 	char c;
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		c = str[i];
+		c = s[i];
 
 		if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
 			c = c + 13;
-		else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) 
-			c = c = - 13;
-		str[i] = c;
+		else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+			c = c - 13;
+		s[i] = c;
 	}
 }
