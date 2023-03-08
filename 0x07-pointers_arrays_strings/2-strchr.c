df29be4413b6d1dbd28ce6da0e03c49b1d910ece
yhammani <yhammani.student@gmail.com>
2023-03-08 07:55:42 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,14 +13,16 @@
 
 char *_strchr(char *s, char c)
 {
-	if (*s == c)
-		return (s);
-
 	while (*s)
 	{
 		if (*s == c)
 			return (s);
 		s++;
+	}
+
+	if (*s == c)
+	{
+		return (s);
 	}
 
 	return (NULL);
