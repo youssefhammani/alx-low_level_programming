3fdd918fab1333a59f1ac431da3ef98aa0befe0b
yhammani <yhammani.student@gmail.com>
2023-03-08 10:05:29 -0800

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: the row of the array
+ *
+ * Return: nothing
+ */
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
