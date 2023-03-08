944e73a88102cdd744965ceaeccf99fa0cca5b54
yhammani <yhammani.student@gmail.com>
2023-03-08 08:30:18 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,19 +13,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
+		if (*s == '\0')
+			return (NULL);
 		s++;
 	}
 
-	if (*s == c)
-	{
-		return (s);
-	}
-
-	return (NULL);
+	return (s);
 }
