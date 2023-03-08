dfb4673dce2574d357d25d2457f350ab5184d5ab
yhammani <yhammani.student@gmail.com>
2023-03-08 11:36:16 -0800

    Double pointer, double fun

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ unsigned int _strspn(char *s, char *accept);
 
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 int _putchar(char c);
 
