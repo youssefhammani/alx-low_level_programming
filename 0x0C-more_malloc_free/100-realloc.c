cea94eb89c8c704de648afd9b84a652632aba261
yhammani <yhammani.student@gmail.com>
2023-03-16 07:10:42 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
 /*
  * This function is a custom implementation of the realloc function in C,
  * which is used to resize a previously allocated block of memory.
