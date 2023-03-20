8429eee2b8aa28a3d0882cd1c61aba98c79f4358
yhammani <yhammani.student@gmail.com>
2023-03-20 08:06:04 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1, s2 + 1));
 	}
 
-	if (*s1 == *s2)
+	if (*s1 != '\0' && (*s1 == *s2 || *s2 == '?'))
 		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
