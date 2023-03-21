022f5be3570b666577e383b17d9c1df8178b501d
yhammani <yhammani.student@gmail.com>
2023-03-21 05:49:46 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -24,37 +24,26 @@ void print_buffer(char *b, int size)
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
-			{
 				printf("%02x", b[i + j]);
-			}
 			else
-			{
 				printf("  ");
-			}
 			if (j % 2 == 1)
-			{
 				printf(" ");
-			}
 		}
-		
+
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 			{
 				char c = b[i + j];
+
 				if (c >= 32 && c <= 126)
-				{
 					printf("%c", c);
-				}
 				else
-				{
 					printf(".");
-				}
 			}
 			else
-			{
 				printf(" ");
-			}
 		}
 		printf("\n");
 	}
