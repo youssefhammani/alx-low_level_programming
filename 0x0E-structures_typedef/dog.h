53e7772bf7341a7cd0e128cbf4845201a3efa599
yhammani <yhammani.student@gmail.com>
2023-03-22 07:24:41 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,8 @@
 #define _DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - structure for a dog
@@ -19,5 +21,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
