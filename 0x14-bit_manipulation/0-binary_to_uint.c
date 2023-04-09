6a8374f929593f592e231182332360286fc01b35
yhammani <yhammani.student@gmail.com>
2023-04-09 16:11:14 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	unsigned int i;
+	unsigned int result = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '0' || b[i] == '1')
-			n = (n << 1) | (b[i] - '0');
+		if (b[i] == '0')
+			result = result * 2;
+		else if (b[i] == '1')
+			result = result * 2 + 1;
 		else
 			return (0);
 	}
 
-	return (n);
+	return (result);
 }
