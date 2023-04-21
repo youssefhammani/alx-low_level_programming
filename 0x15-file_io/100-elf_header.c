745d9f182bb7c065698efd283d3291e11cd3c49e
yhammani <yhammani.student@gmail.com>
2023-04-21 08:49:47 -0700

    This program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,14 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	printf("Number of arguments: %d\n", argc);
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d]: %s (address: %p)\n", i, argv[i], (void *)argv[i]);
-	}
-
-	return 1;
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
 }
