5fb0f4958b6ced554d99ac580cae581498905483
yhammani <yhammani.student@gmail.com>
2023-04-28 10:02:58 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 extern printf
-
 section .data
-    msg: db "Hello, Holberton", 10, 0 ; add new line to end of string
-    fmt: db "%s", 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
 
 section .text
-    global _start
-_start:
-    mov rdi,fmt
-    mov rsi,msg
-    xor rax,rax
-    call printf
-    xor rdi,rdi ; set exit status to 0
-    mov rax,60 ; exit syscall number
-    syscall
+	global main
+main:
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
 
+	pop rbp
+	mov rax,0
+	ret
