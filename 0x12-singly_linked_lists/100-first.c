af6c25311825771a11455850bd50c2ed2e4ece58
yhammani <yhammani.student@gmail.com>
2023-04-28 06:58:08 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - prints a message before main function is executed
+ */
+void __attribute__ ((constructor)) before_main()
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
