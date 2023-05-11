01c271809335e64319e583e2be8e43a0d725b207
yuossefhammani <youssefhammani04@gmail.com>
2023-05-11 13:38:37 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,3 +10,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
+
