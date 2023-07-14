037a6eafb464ec26e148375261d6a6fd22e08684
youssefhammani <youssefhammani04gmail.com>
2023-07-14 10:50:57 -0700

    program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * error_exit - Prints an error message and exits with the specified code.
+ * @code: The exit code.
+ * @msg: The error message.
+ */
+void error_exit(int code, const char *msg)
+{
+	dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(code);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The argument count.
+ * @argv: The argument vector.
+ *
+ * Return: 0 on success, or the appropriate exit code on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to");
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file");
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to file");
+
+	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buf, bytes_read);
+		if (bytes_written == -1)
+			error_exit(99, "Error: Can't write to file");
+	}
+
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file");
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd");
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd");
+
+	return (0);
+}
