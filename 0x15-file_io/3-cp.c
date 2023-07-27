f878c03b973cb36fb2e526b947dd90a94a068f29
youssefhammani <youssefhammani04@gmail.com>
2023-07-27 13:56:48 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * err_exit - print error message and exit
@@ -9,8 +9,8 @@
  */
 void err_exit(char *str, char *file, int code)
 {
-    dprintf(STDERR_FILENO, str, file);
-    exit(code);
+	dprintf(STDERR_FILENO, str, file);
+	exit(code);
 }
 
 /**
@@ -22,41 +22,41 @@ void err_exit(char *str, char *file, int code)
  */
 void cp(char *file_from, char *file_to)
 {
-    int fd1, fd2, numread, numwrote;
-    char buffer[1024];
+	int fd1, fd2, numread, numwrote;
+	char buffer[1024];
 
-    fd1 = open(file_from, O_RDONLY);
-    if (fd1 == -1)
-        err_exit("Error: Can't read from file %s\n", file_from, 98);
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		err_exit("Error: Can't read from file %s\n", file_from, 98);
 
-    fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fd2 == -1)
-        err_exit("Error: Can't write to %s\n", file_to, 99);
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		err_exit("Error: Can't write to %s\n", file_to, 99);
 
-    for (numread = 1024; numread == 1024;)
-    {
-        numread = read(fd1, buffer, 1024);
-        if (numread == -1)
-            err_exit("Error: Can't read from file %s\n", file_from, 98);
+	for (numread = 1024; numread == 1024;)
+	{
+		numread = read(fd1, buffer, 1024);
+		if (numread == -1)
+			err_exit("Error: Can't read from file %s\n", file_from, 98);
 
-        numwrote = write(fd2, buffer, numread);
+		numwrote = write(fd2, buffer, numread);
 
-        if (numwrote == -1)
-            err_exit("Error: Can't write to %s\n", file_to, 99);
-    }
+		if (numwrote == -1)
+			err_exit("Error: Can't write to %s\n", file_to, 99);
+	}
 
-    if (numread == -1)
-        err_exit("Error: Can't read from file %s\n", file_from, 98);
-    if (close(fd2) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-        exit(100);
-    }
-    if (close(fd1) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-        exit(100);
-    }
+	if (numread == -1)
+		err_exit("Error: Can't read from file %s\n", file_from, 98)
+			if (close(fd2) == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+				exit(100);
+			}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
 }
 
 /**
@@ -67,14 +67,14 @@ void cp(char *file_from, char *file_to)
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    cp(argv[1], argv[2]);
+	cp(argv[1], argv[2]);
 
-    return (0);
+	return (0);
 }
 
