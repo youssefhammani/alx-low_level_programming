afefb8ee14ef2d20584b8f4ab2579c60f72949bd
youssefhammani <youssefhammani04@gmail.com>
2023-08-24 07:35:31 -0700

    Delete at index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,5 +31,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 void free_dlistint(dlistint_t *head);
 
 int sum_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
