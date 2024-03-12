9938d60a8a6e208a047d0d8f7756b45e191acb50
youssefhammani <youssefhammani04@gmail.com>
2024-03-12 06:56:53 -0700

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
